4ff6a45fa0ef33944f162c397b4dfa92ef9b6262
pauweezydesign <weezy@paulweezydesign.com>
2022-06-03 04:18:19 -0700

    Initial commit

---
+++ svelte-tailwind-navbar-starter/src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  import '../app.css';
+</script>
+ <a href="/">Home</a> | <a href="/">About</a> | <a href="/">Contact</a>
+<slot />
+<style>
+
+  a {
+    @apply text-violet-500 focus:text-violet-600 text-xl;
+  }
+</style>
---
+++ svelte-tailwind-navbar-starter/src/routes/about.svelte
@@ -0,0 +1,3 @@
+<h1 class="text-9xl text-accent text-center font-medium">About us</h1>
+<p class="text-xl text-center">
+  We are a team of developers and designers who are passionate about creating beautiful and intuitive user <experiences class="">blash</experiences></p>
